be5731ef0f863d0ab68240e5da364ccadc6fd47f
Kartik Kurupaswamy <98091442+kk-kartik@users.noreply.github.com>
2022-08-04 19:44:08 +0530

    v 1.0.2

--- script/vote.js
+++ script/vote.js
@@ -1,25 +1,3 @@
-var mVote = document.getElementById("mVote");
-var verVoter = document.getElementById("verVoter");
-var verVoter2 = document.getElementById("verVoter2");
-var cVote = document.getElementById("cVote");
 
-function full() {
-    mVote.style.display = "none";
-    verVoter.style.display = "none";
-    verVoter2.style.display = "none";
-    cVote.style.display = "none";
-}
-
-function gVer() {
-    full();
-    verVoter.style.display = "flex";
-}
-function proceed() {
-    full();
-    verVoter2.style.display = "flex";
-}
-function proceed2() {
-    full();
-}
 
 
--- script/voting.js
+++ script/voting.js
@@ -11,6 +11,8 @@ const firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 let database = firebase.database();
+//const Web3 = require("web3");
+ // require("dotenv").config();
 firebase.auth().languageCode = 'en';
 window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier('OTP', {
     'size': 'invisible',
@@ -55,6 +57,7 @@ function verify(){
             console.log(error);
             // ...
             });
+            proceed();
         }
     });
     
@@ -63,21 +66,20 @@ function verify(){
 
 function verify2(){
     const code = document.getElementById("OTP").value;
-    var cVote = document.getElementById("cVote");
+    
     window.confirmationResult.confirm(code).then((result) => {
     // User signed in successfully.
-   
+    sessionStorage.setItem('flag',true);
     const user = result.user;
     console.log(user);
-       loadCan();
-        cVote.style.display = "grid";
+    loadCan();
     // ...
     }).catch((error) => {
     // User couldn't sign in (bad verification code?)
     // ...
     console.log(error);
     });
-
+    proceed2();
 }
 
 function loadCan(){
@@ -85,7 +87,6 @@ function loadCan(){
     const $volTemp=$('.canTemplate');
 
     database.ref('Candidates').once('value').then((result)=>{
-       
         result.forEach((res)=>{
             console.log(res);
             console.log(res.val().name);
@@ -93,6 +94,7 @@ function loadCan(){
             const $neT=$volTemp.clone();
             $neT.find('#name').html(res.val().name);
             $neT.find('#pty').html(res.val().pic);
+            $neT.find('#ptpic').attr("src","../assets/"+res.val().pic+".png");
             $('#ccVote').append($neT);
             $neT.show();
         });
@@ -104,21 +106,94 @@ function loadCan(){
             }
         });
     });
-}
-//firebase.database().ref('users').set(firebase.database.ServerValue.increment(1))
+    full();
+    cVote.style.display = "grid";
 
-function castNota(){
+}
+
+async function castNota(){
     const UID=sessionStorage.getItem('UID');
     const VID=sessionStorage.getItem('VID');
-        database.ref('Voting/'+VID).set({
-            name: "NOTA",
-            pty: "NOTA"
-        });
-        database.ref('Voters/'+UID+'/voted').set(true);
-        location.replace("wait.html");
+    await  Vote.cast(VID,"NOTA","NOTA");
+    database.ref('Voters/'+UID+'/voted').set(true);
+    location.replace("wait.html");
 }
 
-function castVote(){
+Vote = {
+  cast: async (VID,cName,cPTY) =>{
+    await Vote.account();
+    const iface = new ethers.utils.Interface(['function castVote(string memory _voterID,string memory _voteCan,string memory _votePty)'])
+    const data = iface.encodeFunctionData('castVote', [VID,cName,cPTY])
+    const tx = {
+      to: '0xC61ffd45058A20FECebB1452a6Ba617CFF3BD2d0',
+      data: data,
+      gas: '3000000',
+      schedule: 'fast'
+    }
+    const signature = await Vote.signRequest(tx)
+    const relayTransactionHash = await Vote.itx.send('relay_sendTransaction', [tx, signature])
+    console.log(relayTransactionHash.relayTransactionHash)
+    sessionStorage.setItem('reTx', relayTransactionHash.relayTransactionHash);
+  },
+  getAcc:async ()=>{
+    database.ref('AccCount').on('value',(res)=>{ 
+      console.log(res.val());
+      randomIndex = Math.floor(Math.random() * res.val());
+    console.log(randomIndex++);
+    database.ref("Acc/"+randomIndex).on('value',(snapshot) =>
+      {
+        console.log(snapshot);
+        console.log(snapshot.val());
+        Vote.add = snapshot.val().add;  
+        Vote.pk = snapshot.val().pk;
+      console.log(Vote.add+"=====>>"+Vote.pk);
+      });
+    });
+  },
+  account: async()=>{
+    await Vote.getAcc();
+    Vote.itx=new ethers.providers.InfuraProvider('goerli','9c9bc90c0beb4234911df94dc923a033');
+    Vote.signer=new ethers.Wallet(Vote.pk,Vote.itx);
+    await Vote.getBalance();
+  },
+  getBalance: async()=>{
+    response=await Vote.itx.send('relay_getBalance',[Vote.add]);
+    console.log(response);
+  },
+  signRequest: async (tx)=>{
+    const relayTransactionHash = ethers.utils.keccak256(
+      ethers.utils.defaultAbiCoder.encode(
+        ['address', 'bytes', 'uint', 'uint', 'string'],
+        [tx.to, tx.data, tx.gas, 5, tx.schedule] // Goerli chainId is 5
+      )
+    )
+    return await Vote.signer.signMessage(ethers.utils.arrayify(relayTransactionHash))
+  },
+  waiti:async (tx)=>{
+    let mined = false
+    await Vote.account();
+  while (!mined) {
+    const statusResponse = await Vote.itx.send('relay_getTransactionStatus', [tx])
+
+    if (statusResponse.broadcasts) {
+      for (let i = 0; i < statusResponse.broadcasts.length; i++) {
+        const bc = statusResponse.broadcasts[i]
+        const receipt = await Vote.itx.getTransactionReceipt(bc.ethTxHash)
+        if (receipt && receipt.confirmations && receipt.confirmations > 1) {
+          mined = true
+          return receipt
+        }
+      }
+    }
+    await Vote.waitkaro(1000)
+  }
+  },
+  waitkaro:async (milliseconds) => {
+    return new Promise((resolve) => setTimeout(resolve, milliseconds))
+  },
+};
+async function castVote(){
+    
     var can=document.querySelector('input[type="radio"]:checked');
     console.log(can);
     var text=can.parentElement.innerText;
@@ -130,22 +205,38 @@ function castVote(){
     console.log(cName+" => "+cPTY);
     const UID=sessionStorage.getItem('UID');
     const VID=sessionStorage.getItem('VID');
-        database.ref('Voting/'+VID).set({
-            name: cName,
-            pty: cPTY,
-        });
-        database.ref('Voters/'+UID+'/voted').set(true);
-     firebase.auth().signOut().then(() => {
-      // Sign-out successful.
-    }).catch((error) => {
-      // An error happened.
-    });
-        location.replace("wait.html");
+      await  Vote.cast(VID,cName,cPTY);
+      database.ref('Voters/'+UID+'/voted').set(true);
+      location.replace("wait.html");
+      
 }
 //After Votinf is done by a voter
+async function waiti(tx){
+  let mined = false
+  itx=new ethers.providers.InfuraProvider('goerli','9c9bc90c0beb4234911df94dc923a033');
 
-function wait(){
+while (!mined) {
+  const statusResponse = await itx.send('relay_getTransactionStatus', [tx])
+
+  if (statusResponse.broadcasts) {
+    for (let i = 0; i < statusResponse.broadcasts.length; i++) {
+      const bc = statusResponse.broadcasts[i]
+      const receipt = await itx.getTransactionReceipt(bc.ethTxHash)
+      if (receipt && receipt.confirmations && receipt.confirmations > 1) {
+        mined = true
+        return receipt
+      }
+    }
+  }
+  await waitkaro(1000)
+}
+}
+async function waitkaro(milliseconds) {
+  return new Promise((resolve) => setTimeout(resolve, milliseconds))
+}
+async function wait(){
     const VID=sessionStorage.getItem('VID');
+    reTx=sessionStorage.getItem('reTx');
     
     let success=document.getElementById('center2');
     let wait=document.getElementById('center');
@@ -157,17 +248,18 @@ function wait(){
     let S=sessionStorage.getItem('S');
     console.log(VID);
     if(!sessionStorage['VID']){location.replace("voting.html");} 
-    database.ref('Voted/'+VID).on('value',(res)=>{
-        blid.innerHTML=res.val().Acc;
-        blhx.innerHTML=res.val().Blockhash;
-        trnhx.innerHTML=res.val().TxHash;
+    res=await waiti(reTx);
+        console.log(res);
+        blid.innerHTML=res.from;
+        blhx.innerHTML=res.blockHash;
+        trnhx.innerHTML=res.transactionHash;
         mVid.innerHTML=VID;
         database.ref('data/gender/'+G).set(firebase.database.ServerValue.increment(1));
         database.ref('data/state/'+S+'/'+G).set(firebase.database.ServerValue.increment(1));
         wait.style.display="none";
         startcntdown();
         success.style.display="flex";
-    });
+    
 }
 function startcntdown(){
     let progressBar = document.querySelector(".circular-progress");
@@ -185,7 +277,6 @@ let progress = setInterval(() => {
       rgb(176, 229, 161) ${progressValue * 45}deg
   )`;
     if (progressValue == progressEndValue) {
-       
         sessionStorage.clear();
         location.replace('voting.html');
         clearInterval(progress);
@@ -193,3 +284,28 @@ let progress = setInterval(() => {
 }, speed);
 }
 
+var mVote = document.getElementById("mVote");
+var verVoter = document.getElementById("verVoter");
+var verVoter2 = document.getElementById("verVoter2");
+var cVote = document.getElementById("cVote");
+
+function full() {
+    mVote.style.display = "none";
+    verVoter.style.display = "none";
+    verVoter2.style.display = "none";
+    cVote.style.display = "none";
+}
+
+function gVer() {
+    full();
+    verVoter.style.display = "flex";
+}
+function proceed() {
+    full();
+    verVoter2.style.display = "flex";
+}
+function proceed2() {
+    if (sessionStorage.getItem("flag") == "true") {
+        
+    }
+}
